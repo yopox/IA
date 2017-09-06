1b08f459acf7df10bf1517c9efcdaa0f2f528aa8
yopox <yopoxdev@gmail.com>
2017-09-06 03:26:52 +0200

    Spinning char

--- core/src/com/llgames/ia/IA.kt
+++ core/src/com/llgames/ia/IA.kt
@@ -15,13 +15,16 @@ import com.badlogic.gdx.utils.viewport.FitViewport
 class IA : ApplicationAdapter() {
     private lateinit var batch: SpriteBatch
     private lateinit var bg: Texture
-    private lateinit var viewport: Viewport
+    private lateinit var char: Texture
+    private lateinit var viewport: FitViewport
     private lateinit var camera: OrthographicCamera
+    var angle = 0.toDouble()
 
     override fun create() {
         batch = SpriteBatch()
         bg = Texture("bg.png")
         bg.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat)
+        char = Texture("char.png")
 
         camera = OrthographicCamera()
         viewport = FitViewport(320f, 180f, camera)
@@ -31,13 +34,33 @@ class IA : ApplicationAdapter() {
     }
 
     override fun render() {
+        angle += 0.02
         camera.update()
         val clearColor = Color.getColor("5850c0")
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(49 / 255f, 49 / 255f, 54 / 255f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        var camX = Math.cos(angle)
+        var camY = Math.sin(angle)
+        var oX = camera.viewportWidth / 2
+        var oY = camera.viewportHeight / 2
+        var charX = 0.45f
+        var charY = 0.3f
+
+        var beta = ((charY - camY.toFloat()) angle (charX - camX.toFloat())).toDouble()
+        var dist = Math.sqrt(Math.pow(charY.toDouble(), 2.0) + Math.pow(charX.toDouble(), 2.0))
+
+        var posX = (oX - dist * Math.sin(beta) * 140 - 8).toFloat() - 16
+        var posY = (oY - dist * Math.cos(beta) * 30 - 8).toFloat()
+
         batch.projectionMatrix = camera.combined
         batch.begin()
-        batch.draw(bg, 0f, 78f, 128, 0, 320, 102)
+        batch.draw(bg, 0f, 78f, (-64 * angle).toInt(), 0, 320, 102)
+        if (posX + 16 < oX) {
+            batch.draw(char, posX + 16, posY,-16f,32f)
+        } else {
+            batch.draw(char, posX, posY)
+        }
         batch.end()
     }
 
@@ -45,4 +68,9 @@ class IA : ApplicationAdapter() {
         batch.dispose()
         bg.dispose()
     }
+
+    private infix fun Float.angle(x:Float): Float {
+        return Math.atan2(this.toDouble(), x.toDouble()).toFloat();
+    }
+
 }
--- desktop/src/com/llgames/ia/desktop/DesktopLauncher.kt
+++ desktop/src/com/llgames/ia/desktop/DesktopLauncher.kt
@@ -1,16 +1,16 @@
+@file:JvmName("DesktopLauncher")
+
 package com.llgames.ia.desktop
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import com.llgames.ia.IA
 
-object DesktopLauncher {
-    @JvmStatic fun main(arg: Array<String>) {
-        var config = LwjglApplicationConfiguration().apply {
-            title = "IA Desktop"
-            width = 1280
-            height = 720
-        }
-        LwjglApplication(IA(), config)
+fun main(arg: Array<String>) {
+    val config = LwjglApplicationConfiguration().apply {
+        title = "IA Desktop"
+        width = 320
+        height = 180
     }
+    LwjglApplication(IA(), config)
 }
